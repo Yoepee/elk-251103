f0a6762738c0f04c00f1b6e8a33fedaf5d567316
Yoepee <kdy2975@naver.com>
2025-11-03 11:38:08 +0900

    키바나 추가

--- src/main/java/com/back/BackApplication.java
+++ src/main/java/com/back/BackApplication.java
@@ -2,14 +2,12 @@ package com.back;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
-@EnableJpaAuditing
-public class PtElk251103Application {
+public class BackApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PtElk251103Application.class, args);
+		SpringApplication.run(BackApplication.class, args);
 	}
 
 }
--- src/main/java/com/back/domain/post/postDoc/document/PostDoc.java
+++ src/main/java/com/back/domain/post/postDoc/document/PostDoc.java
@@ -1,9 +1,9 @@
 package com.back.domain.post.postDoc.document;
 
-import jakarta.persistence.Id;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.elasticsearch.annotations.Document;
 
 @Document(indexName = "app1_posts")
